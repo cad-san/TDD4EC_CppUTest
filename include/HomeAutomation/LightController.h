cec4961539579ec2f9695d3df332d71eea17b023
cad-san <cadsan1986@gmail.com>
2012-06-03 09:58:44 +0900

    impremented LightControllSpy

--- include/HomeAutomation/LightController.h
+++ include/HomeAutomation/LightController.h
@@ -3,5 +3,7 @@
 
 void LightController_Create(void);
 void LightController_Destroy(void);
+void LightController_On(int id);
+void LightController_Off(int id);
 
 #endif
--- tests/HomeAutomation/LightControllerSpy.c
+++ tests/HomeAutomation/LightControllerSpy.c
@@ -13,6 +13,18 @@ void LightController_Destroy(void)
 {
 }
 
+void LightController_On(int id)
+{
+    lastId = id;
+    lastState = LIGHT_ON;
+}
+
+void LightController_Off(int id)
+{
+    lastId = id;
+    lastState = LIGHT_OFF;
+}
+
 int LightControllerSpy_GetLastId(void)
 {
     return lastId;
